632e6e326c55cf34fdb8bd8e7ba1cc936a6812c7
Carturo8 <caarojasbo@unal.edu.co>
2025-10-24 17:21:35 -0500

    Add Todo model class for task management

---
+++ backend/src/main/java/com/crud/tinytasks/model/Todo.java
@@ -0,0 +1,49 @@
+package com.crud.tinytasks.model;
+
+public class Todo {
+    private int id;
+    private String title;
+    private boolean done;
+
+    public Todo() {
+    }
+
+    public Todo(int id, String title, boolean done) {
+        this.id = id;
+        this.title = title;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", done=" + done +
+                '}';
+    }
+}
